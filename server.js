2046e414437f952d9716e386fd17aec2b668f27a
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-04 18:11:24 -0500

    reconfiguring dependencies

--- server.js
+++ server.js
@@ -1,91 +1,78 @@
-// Dependencies
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const uuid = require("uuid/v1");
+// This sets up the basic properties for our express server
+var express = require("express");
+var path = require("path");
+var fs = require('fs');
+var util = require('util');
 
-// Sets up the Express App
-const app = express();
-const PORT = process.env.PORT || 8080;
-const mainDir = path.join(__dirname, "/public");
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
 
-// Routes to HTML Files
-app.get("/", function(req, res) {
-    res.sendFile(path.join(mainDir, "index.html"));
-});
+//Set variables
+const writefileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+let allNotes;
 
+// ROUTER
+// The below points our server to set up routes
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(mainDir, "notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-// Routes to GET, POST and DELETE notes 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.get("/api/notes", function(req, res) {
-    let savedNotes = require("./db/db.json");
-    res.json(savedNotes);
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function(data) {
+            return res.json(JSON.parse(data));
+        });
 });
 
 app.post("/api/notes", function(req, res) {
-    let savedNotes = require("./db/db.json");
-
-    console.log(savedNotes);
-
-    // User input for new note object
-    let newNote = req.body;
-    console.log(newNote);
-
-    // Adds unique id to saved notes
-    newNote.id = uuid();
-
-    savedNotes.push(newNote);
-
-    savedNotes = JSON.stringify(savedNotes);
-
-    // create writeFile function to save new notes  
-    fs.writeFile("./db/db.json", savedNotes, "utf8", (err) => {
-        if (err) throw err;
-        console.log("New Note written to file");
-
-        res.json(JSON.parse(savedNotes));
-    });
-
+    var newNote = req.body;
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function(data) {
+            allNotes = JSON.parse(data);
+            if (newNote.id || newNote.id === 0) {
+                let currNote = allNotes[newNote.id];
+                currNote.title = newNote.title;
+                currNote.text = newNote.text;
+            } else {
+                allNotes.push(newNote);
+            }
+            writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes))
+                .then(function() {
+                    console.log("Wrote db.json");
+                })
+        });
+    res.json(newNote);
 });
 
 app.delete("/api/notes/:id", function(req, res) {
-    let id = req.params.id;
-    console.log(id);
-
-    let savedNotes = require("./db/db.json");
-
-    for (let i = 0; i < savedNotes.length; i++) {
-        if (id === savedNotes[i].id) {
-            savedNotes.splice(i, 1);
-            console.log(savedNotes);
-        }
-    }
-
-    savedNotes = JSON.stringify(savedNotes);
-
-    // create writeFile function to save new array of saved notes  
-    fs.writeFile("./db/db.json", savedNotes, "utf8", (err) => {
-        if (err) throw err;
-        console.log("Note deleted from file");
-    });
-
-    res.send(JSON.parse(savedNotes));
-
+    var id = req.params.id;
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function(data) {
+            allNotes = JSON.parse(data);
+            allNotes.splice(id, 1);
+            writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes))
+                .then(function() {
+                    console.log("Deleted db.json");
+                })
+        });
+    res.json(id);
 });
 
-// Route to home page if any other route entered
-app.get("*", function(req, res) {
-    res.sendFile(path.join(mainDir, "index.html"));
-});
-
-// Starts the server to begin listening
+// LISTENER
+// The below code effectively "starts" our server
 app.listen(PORT, function() {
-    console.log(`App is listening on http://localhost:${PORT}`);
+    console.log("App listening on PORT: " + PORT);
 });
