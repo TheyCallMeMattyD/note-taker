4b0e11d337e0fe38be19b9dbcc875a0c5eb0db15
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-04 19:35:09 -0500

    what a clustery mess

--- server.js
+++ server.js
@@ -1,23 +1,91 @@
-// add required modules
+// Dependencies
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid/v1");
 
-// Create server application at port 3000
+// Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+const mainDir = path.join(__dirname, "/public");
 
-// Read URL or JSON
+// Sets up the Express app to handle data parsing
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Include js files
-require("routes/apiRoutes.js")(app);
-require("routes/htmlRoutes")(app);
+// Routes to HTML Files
+app.get("/", function(req, res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+});
 
-// Use public folder
-app.use(express.static("public"));
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
 
-// Add listener
+// Routes to GET, POST and DELETE notes 
+app.get("/api/notes", function(req, res) {
+    let savedNotes = require("./db/db.json");
+    res.json(savedNotes);
+});
+
+app.post("/api/notes", function(req, res) {
+    let savedNotes = require("./db/db.json");
+
+    console.log(savedNotes);
+
+    // User input for new note object
+    let newNote = req.body;
+    console.log(newNote);
+
+    // Adds unique id to saved notes
+    newNote.id = uuid();
+
+    savedNotes.push(newNote);
+
+    savedNotes = JSON.stringify(savedNotes);
+
+    // create writeFile function to save new notes  
+    fs.writeFile("./db/db.json", savedNotes, "utf8", (err) => {
+        if (err) throw err;
+        console.log("New Note written to file");
+
+        res.json(JSON.parse(savedNotes));
+    });
+
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    let id = req.params.id;
+    console.log(id);
+
+    let savedNotes = require("./db/db.json");
+
+    for (let i = 0; i < savedNotes.length; i++) {
+        if (id === savedNotes[i].id) {
+            savedNotes.splice(i, 1);
+            console.log(savedNotes);
+        }
+    }
+
+    savedNotes = JSON.stringify(savedNotes);
+
+    // create writeFile function to save new array of saved notes  
+    fs.writeFile("./db/db.json", savedNotes, "utf8", (err) => {
+        if (err) throw err;
+        console.log("Note deleted from file");
+    });
+
+    res.send(JSON.parse(savedNotes));
+
+});
+
+// Route to home page if any other route entered
+app.get("*", function(req, res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+});
+
+// Starts the server to begin listening
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log(`App is listening on http://localhost:${PORT}`);
 });
