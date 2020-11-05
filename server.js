af11093c91835201dbdea3dc3b5782c440a15aef
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-04 19:09:58 -0500

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 // const path = require('path');
 // const fs = require('fs');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
 
 //Testing, not really needed here
 const dbJson = require('./db/db.json')
